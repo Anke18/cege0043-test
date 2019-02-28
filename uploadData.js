069c86477da2b7c0cc620aa4c2888c487a29b70b
Anke18 <uczlaw3@ucl.ac.uk>
2019-02-28 14:18:58 -0500

    Add AJAX request

--- uploadData.js
+++ uploadData.js
@@ -1,3 +1,5 @@
+var client;
+
 function startDataUpload() 
 {
 	var name = document.getElementById("name").value;
@@ -7,3 +9,18 @@ function startDataUpload()
 	alert (postString);
 }
 
+function processData(postString)
+{
+	client = new XMLHttpRequest();
+	client.open('POST','http://developer.cege.ucl.ac.uk:30312/reflectData',true);
+	client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	client.onreadystatechange = dataUploaded;
+	client.send(postString);
+}
+
+function dataUploaded()
+{
+	if (client.readyState == 4){
+		document.getElementById("dataUploadResult").innerHTML = client.responseText;
+	}
+}
